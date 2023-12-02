3b264fe0f8f09b889a34db5cc14413067d0dd838
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2023-12-02 15:02:12 -0500

    added some of Sterling's css

--- login.php
+++ login.php
@@ -1,15 +1,16 @@
 <?php
+
 session_start();
 
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$databasename = 'dolphin_crm';
+$dbname = 'dolphin_crm';
 
-$message = ''; // To store error messages
+$message = '';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$databasename", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -29,38 +30,45 @@ try {
             $message = "Invalid username or password";
         }
     }
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     $message = "Connection failed: " . $e->getMessage();
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="login.css" />
 </head>
+
+<?php include "header.php" ?>
+
 <body>
     <h2>Login</h2>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <div>
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email" required />
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-        </div>
-        <div>
-            <input type="submit" value="Login" />
-        </div>
-    </form>
-
+    <div class="login-container">
+        <form class="form-login" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <div class="input-container">
+                <label for="email">Email:</label>
+                <input class="form-control" type="text" id="email" name="email" required />
+            </div>
+            <div class="input-container">
+                <label for="password">Password:</label>
+                <input class="form-control" type="password" id="password" name="password" required />
+            </div>
+            <div class="input-container">
+                <input class="form-control" type="submit" value="Login" />
+            </div>
+        </form>
+    </div>
     <?php if ($message) { ?>
-        <p><?php echo $message; ?></p>
+        <p>
+            <?php echo $message; ?>
+        </p>
     <?php } ?>
 </body>
+
 </html>
